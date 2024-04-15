ca975580d71f7ee703e1288bbc139a4ae15f8934
harishperni <harishperni007@gmail.com>
2024-04-15 18:06:06 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
-import SearchBar from './SearchBar'; // Importing the SearchBar component
-import Footer from './components/Footer'; // Importing the Footer component
-import axios from 'axios'; // Importing axios for making HTTP requests
-//import Navbar from './components/Navbar'; // Import the Navbar component
-import './App.css'; // Importing the CSS file for styling
+import SearchBar from './SearchBar';
+import Footer from './components/Footer';
+import axios from 'axios';
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
-  // State variables to manage search results and errors
   const [results, setResults] = useState([]);
   const [error, setError] = useState('');
 
-  // Function to handle form submissions
   const handleSubmit = async (query) => {
     try {
-      // Clear previous results
       setResults([]);
-      // Send a GET request to the server with the search query
       const response = await axios.get(`http://localhost:5002/structures?q=${query}`);
-      // Update state with the received data if there are results
       if (response.data.length === 0) {
         setError('No results found for the specified query.');
       } else {
@@ -25,7 +20,6 @@ function App() {
         setError('');
       }
     } catch (error) {
-      // Handle errors if the request fails
       console.error('Error fetching data:', error);
       setError('An error occurred while fetching data.');
     }
@@ -33,32 +27,27 @@ function App() {
 
   return (
     <div className="container">
-            <Navbar/> {/* Include the Navbar component */}
-
+      <Navbar />
       <div className="App">
-        {/* Display the main heading */}
         <h1>Structure Search</h1>
-        {/* Render the SearchBar component and pass the handleSubmit function */}
         <SearchBar onSubmit={handleSubmit} />
-        {/* Display error message if there is an error */}
         {error && <p className="error">{error}</p>}
-        {/* Render the search results */}
-        <ul>
+        <div className="tiles-container">
           {results.map((structure, index) => (
-            <li key={index}>
-              {/* Display details of each structure */}
-              <p>Structure ID: {structure.structure_id}</p>
-              <p>Structure Type: {structure.structure_type}</p>
-              <p>User ID: {structure.user_id}</p>
-              <p>Tags: {structure.tags.join(', ')}</p>
-              <p>Price: ${structure.price}</p>
-              <p>Number of Rooms: {structure.number_of_rooms}</p>
-              <p>Designed by: {structure.designed_by}</p>
-              <p>Dimensions in sq.ft: {structure.dimensions_in_sqft}</p>
-            </li>
+            <a key={index} href={`https://www.google.com`} className="tile-link">
+              <div className="tile">
+                <p>Structure ID: {structure.structure_id}</p>
+                <p>Structure Type: {structure.structure_type}</p>
+                <p>User ID: {structure.user_id}</p>
+                <p>Tags: {structure.tags.join(', ')}</p>
+                <p>Price: ${structure.price}</p>
+                <p>Number of Rooms: {structure.number_of_rooms}</p>
+                <p>Designed by: {structure.designed_by}</p>
+                <p>Dimensions in sq.ft: {structure.dimensions_in_sqft}</p>
+              </div>
+            </a>
           ))}
-        </ul>
-        {/* Render the Footer component */}
+        </div>
         <Footer />
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
-// components/Navbar.js
+// Navbar.js
 import React from 'react';
 import './Navbar.css';
-
-
+import caterpillarLogo from '/Users/harishperni/test-app/client/src/components/caterpillar_logo.png';
 
 const Navbar = () => {
   return (
     <header className="Navbar">
-      <img src='/caterpillar-logo.png' alt="Caterpillar Logo" />
+      <img src={caterpillarLogo} alt="Caterpillar Logo" />
       {/* Add navigation links or logo here */}
     </header>
   );
